9225df055b686ad93dfcc0e3fcf43e12aac79f35
AshiswhoIam <71729682+AshiswhoIam@users.noreply.github.com>
2023-11-08 15:57:45 -0500

    fixed adding cart stuff and getting data

--- epay/src/main/java/servlets/cartServlet.java
+++ epay/src/main/java/servlets/cartServlet.java
@@ -5,11 +5,10 @@ import java.io.PrintWriter;
 import java.util.ArrayList;
 import business.Cart;
 import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpSession;
 
 @jakarta.servlet.annotation.WebServlet("/cart")
-public class cartServlet extends HttpServlet{
+public class cartServlet extends jakarta.servlet.http.HttpServlet{
 
     protected void doGet(jakarta.servlet.http.HttpServletRequest request,jakarta.servlet.http.HttpServletResponse response) throws IOException, ServletException {
         response.setContentType("text/html;charset=UTF-8");
@@ -22,33 +21,44 @@ public class cartServlet extends HttpServlet{
             cartObj.setSku(sku);
             cartObj.setQuantity(1);
 
+        
+            
             HttpSession session = request.getSession();
             ArrayList<Cart> cl =  (ArrayList<Cart>) session.getAttribute("somelist");
 
             if (cl == null) {
                 cList.add(cartObj);
                 session.setAttribute("somelist", cList);
-                response.sendRedirect("index.jsp");
+              
                  out.println("testing sessions works ");
-            }else{
-
-                cList = cl;
-                 boolean stop = false;
-                 for (Cart c: cl) {
-                    if (c.getSku() == sku) {
-                        stop = true;
-                        out.println("<h3 text-align: center'>Thing is already in yo Cart. <a href='cart.jsp'>Travel to CART</a></h3>");
-                    }
-                }
-
-            
-
-            if (!stop) {
-                cList.add(cartObj);
-                response.sendRedirect("index.jsp");
             }
+            else{
 
-        }
+                out.println("testing sessions if lands here ti will works.works ");
+                cList = cl;
+                boolean stop = false;
+
+                
+                for(Cart c: cList){
+
+                    if (c.getSku()==sku){
+                        stop=true;
+                        out.println("<h3 text-align: center'>Thing is already in yo Cart. <a href='cart.jsp'>Travel to CART</a></h3>");
+
+
+                    }
+
+                }
+                        if(!stop){
+                        cl.add(cartObj);
+                        response.sendRedirect("index.jsp");
+                        out.println("lets see if we added the prodcus.");
+
+                    }
+            }
+             for(Cart c: cList){
+                out.println(c.getSku());
+             }
 
 
 
