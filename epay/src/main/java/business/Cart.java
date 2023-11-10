9edbf22aca56f515770eb3e65eab0361cd76b307
AshiswhoIam <71729682+AshiswhoIam@users.noreply.github.com>
2023-11-10 15:50:07 -0500

    some additions to the business layer more soon

--- epay/src/main/java/business/Cart.java
+++ epay/src/main/java/business/Cart.java
@@ -7,10 +7,20 @@ public class Cart extends Product {
 
 	private int quantity;
 	private String shippingAddress;
-	private List<String> allProducts = new ArrayList<>();
+	public long userId;
+	private List<Product> allProducts = new ArrayList<>();
 
 	public Cart() {
 	}
+
+	public Cart(long userId) {
+        this.userId = userId;
+        this.allProducts = new ArrayList<>();
+    }
+
+	public List<Product> getAllProducts() {
+        return allProducts;
+    }
 
 	public int getQuantity() {
 		return quantity;
@@ -27,4 +37,22 @@ public class Cart extends Product {
 	public void setShippingAddress(String shippingAddress) {
 		this.shippingAddress = shippingAddress;
 	}
+
+	public int getUserId(long userId) {
+		this.userId = userId;
+		return (int) userId;
+	}
+
+	public void addProduct(Product product) {
+        allProducts.add(product);
+    }
+
+    public void removeProduct(int sku) {
+		allProducts.remove(sku);
+    }
+
+
+	
+
+
 }
