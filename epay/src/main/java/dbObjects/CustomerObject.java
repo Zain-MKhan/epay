1a659bc0322b27a86c3d00ffe6fb8ca8af598c0c
Zain-MKhan <zmkhan025@gmail.com>
2023-11-10 21:13:24 -0500

    json parsing + local file for customer passwords

--- epay/src/main/java/dbObjects/CustomerObject.java
+++ epay/src/main/java/dbObjects/CustomerObject.java
@@ -3,8 +3,19 @@ package dbObjects;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.Scanner;
 
 import business.Customer;
+
+import java.io.FileReader;
+//The following imports are for json customer authentication:
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 
 public class CustomerObject {
     private Connection connection;
@@ -18,18 +29,46 @@ public class CustomerObject {
 
     public Customer customerLogin(String email, String password) {
         Customer customer = null;
+        // String json = "";
         try {
-            myQuery = "select * from customers where email=? and password=?";
-            preparedStatement = this.connection.prepareStatement(myQuery);
-            preparedStatement.setString(1, email);
-            preparedStatement.setString(2, password);
-            resultSet = preparedStatement.executeQuery();
 
-            if (resultSet.next()) {
-                customer = new Customer();
-                customer.setEmail(resultSet.getString("email"));
-                customer.setPassword(resultSet.getString("password"));
+            // ******THE FOLLOWING IS IF WE WANT TO CHECK DATABASE AND NOT JSON FILE******
+            // myQuery = "select * from customers where email=? and password=?";
+            // preparedStatement = this.connection.prepareStatement(myQuery);
+            // preparedStatement.setString(1, email);
+            // preparedStatement.setString(2, password);
+            // resultSet = preparedStatement.executeQuery();
+
+            // if (resultSet.next()) {
+            // customer = new Customer();
+            // customer.setEmail(resultSet.getString("email"));
+            // customer.setPassword(resultSet.getString("password"));
+            // }
+            // ***************************************************************************
+
+            // ************THE FOLLOWING IS IF WE WANT TO CHECK THE JSON FILE*************
+            // Read the JSON file into a string
+            String filePath = "CustomerPasswords.json";
+            String json = new String(
+                    Files.readAllBytes(Paths.get(getClass().getClassLoader().getResource(filePath).toURI())));
+
+            JsonElement element = JsonParser.parseString(json);
+            JsonArray jsonArray = element.getAsJsonArray();
+
+            for (JsonElement user : jsonArray) {
+                JsonObject jsonObject = user.getAsJsonObject();
+                String storedEmail = jsonObject.get("email").getAsString();
+                String storedPassword = jsonObject.get("password").getAsString();
+
+                if (email.equals(storedEmail) && password.equals(storedPassword)) {
+                    customer = new Customer();
+                    customer.setEmail(email);
+                    customer.setPassword(password);
+                    break;
+                }
             }
+            // ***************************************************************************
+
         } catch (Exception e) {
             e.printStackTrace();
         }
